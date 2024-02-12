b82aae72d4af71d3d7468d8240f8bcf4dd8f560f
Marcus Reinhardt <marcus.reinhardt@deutschebahn.com>
2024-02-12 14:52:53 +0100

    update demo

--- demo.js
+++ demo.js
@@ -4,7 +4,10 @@ const kTestRunner = ["node", "tap", "tape", "vitest", "mocha", "ava"];
 
 const name = await question("Project name ?", { defaultValue: "foo" });
 const runner = await select("Choose a test runner", { choices: kTestRunner, maxVisible: 5 });
-const runner_complete = await select("Choose a test runner", { choices: kTestRunner, maxVisible: 5, autocomplete: true });
+const runner_complete = await select(
+  "Choose a test runner (autocomplete)",
+  { choices: kTestRunner, maxVisible: 5, autocomplete: true }
+);
 const isCLI = await confirm("Your project is a CLI ?", { initial: true });
 const os = await multiselect("Choose OS", {
   choices: ["linux", "mac", "windows"],
@@ -16,11 +19,14 @@ const agent = PromptAgent.agent();
 agent.nextAnswer(["toto", "tap", "tape", false, "linux"]);
 
 const _name = await question("Project name ?", { defaultValue: "foo" });
-// const _runner = await select("Choose a test runner", { choices: kTestRunner, maxVisible: 5 });
-const _runner_complete = await select("Choose a test runner", { choices: kTestRunner, maxVisible: 5, autocomplete: true });
+const _runner = await select("Choose a test runner", { choices: kTestRunner, maxVisible: 5 });
+const _runner_complete = await select(
+  "Choose a test runner (autocomplete)",
+  { choices: kTestRunner, maxVisible: 5, autocomplete: true }
+);
 const _isCLI = await confirm("Your project is a CLI ?", { initial: true });
 const _os = await multiselect("Choose OS", {
   choices: ["linux", "mac", "windows"],
   preSelectedChoices: ["linux"]
 });
-console.log(_name, _runner_complete, _isCLI, _os);
+console.log(_name, _runner, _runner_complete, _isCLI, _os);
