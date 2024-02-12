564736b0985587adcf1a9830e4d36ee89af94cb7
Marcus Reinhardt <marcus.reinhardt@deutschebahn.com>
2024-02-12 16:07:56 +0100

    update demo script

--- demo.js
+++ demo.js
@@ -15,21 +15,34 @@ const interactive = await confirm("Interactive demo?", { initial: true });
 
 if (!interactive) {
   const agent = PromptAgent.agent();
-  agent.nextAnswer(["toto", "node", "tape", false, "node", "tap, tape"]);
+  agent.nextAnswer([
+    // Question input
+    "toto",
+    // Select input
+    "node",
+    // Select input /w autocomplete
+    "tape",
+    // Confirm input
+    false,
+    // Multiselect input
+    "node",
+    // Multiselect input /w autocomplete
+    ["tap", "tape"]
+  ]);
 }
 
-const name = await question("Project name ?", { defaultValue: "foo" });
-const runner = await select("Choose a test runner", { choices: stringSelect, maxVisible: 5 });
+const name = await question("Question input", { defaultValue: "foo" });
+const runner = await select("Select input", { choices: stringSelect, maxVisible: 5 });
 const runner_complete = await select(
-  "Choose a test runner (autocomplete)",
+  "Select input /w autocomplete",
   { choices: choiceSelect, maxVisible: 5, autocomplete: true }
 );
-const isCLI = await confirm("Your project is a CLI ?", { initial: true });
-const os = await multiselect("Choose OS", {
+const isCLI = await confirm("Confirm input", { initial: true });
+const os = await multiselect("Multiselect input", {
   choices: stringSelect,
   preSelectedChoices: ["node"]
 });
-const os_autocomplete = await multiselect("Choose OS (autocomplete)", {
+const os_autocomplete = await multiselect("Multiselect input /w autocomplete", {
   choices: choiceSelect,
   preSelectedChoices: ["tap"]
 });
